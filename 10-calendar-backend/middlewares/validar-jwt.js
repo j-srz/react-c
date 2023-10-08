0ad4cf8e1e94be2cfdac17ca9908176c69afe4ae
Jesus Suarez <jesucal68@gmail.com>
2023-10-07 21:36:55 -0600

    Fin seccion 24 inicio del back-end de la app

---
+++ 10-calendar-backend/middlewares/validar-jwt.js
@@ -0,0 +1,51 @@
+const { response } = require("express")
+const jwt = require('jsonwebtoken');
+
+const validarJWT = ( req, res = response, next) => {
+
+    // x-token
+
+    const token = req.header('x-token');
+
+
+    if ( !token ) {
+        return res.status(401).json({
+            ok: false,
+            msg: 'No hay token en la petición'
+        })
+    }
+
+    try {
+
+        const { name, uid } = jwt.verify(
+            token,
+            process.env.SECRET_JWT_SEED
+        );
+
+
+        req.uid = uid;
+        req.name = name;
+        
+
+
+
+        
+    } catch (error) {
+        return res.status(401).json({
+            ok: false,
+            msg: 'Token no valido'
+        })
+    }
+
+
+
+
+    next();
+}
+
+
+
+
+module.exports = {
+    validarJWT
+}
---
+++ 10-calendar-backend/routes/auth.js
@@ -0,0 +1,43 @@
+/*
+    Rutas de Usuarios / Auth
+    host + api/auth
+*/
+
+const express = require('express');
+const { check } = require('express-validator');
+
+const router = express.Router();
+
+const { crearUsuario, revalidarToken, loginUsuario } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+router.post(
+    '/new', 
+    [
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de der de 6 caracteres').isLength({min: 6}),
+        validarCampos
+    ],
+    crearUsuario,
+)
+
+router.post(
+    '/', 
+    [
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'EL password debe ser mayor a 6 caracteres').isLength({min: 6}),
+        validarCampos
+    ],
+    loginUsuario,
+)
+
+router.get(
+    '/renew', 
+    validarJWT,
+    revalidarToken,
+)
+
+
+module.exports = router;
